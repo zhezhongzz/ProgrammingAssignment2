a27e140d4a4492e22e88d2995bebf24aaff92e4e
zhezhongzz <52753384+zhezhongzz@users.noreply.github.com>
2020-02-13 12:19:58 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         inver = NULL
-        set <- function(y){
+        set <- function(y){      ## set the matrix created
                 x <<- y
                 inver <<- NULL
         }
-        get <- function()x
-        setinversion <- function(inversion) inver <<- inversion
-        getinversion <- function() inver
-        list(set = set, get = get, setinversion = setinversion, getinversion = getinversion)
+        get <- function()x       ## get the matrix created
+        setinversion <- function(inversion) inver <<- inversion  ## set the inversion of the matrix
+        getinversion <- function() inver  ## get the inversion of the matrix
+        list(set = set,
+             get = get, 
+             setinversion = setinversion, 
+             getinversion = getinversion)  ## return the the list of four functions defined above
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         inver <- x$getinversion()
-        if(!is.null(inver)){
+        if(!is.null(inver)){           ## test whether the inversion has been calculated and cached
                 message("getting cached matrix")
                 return(inver)
         }
-        matrix <- x$get()
+        matrix <- x$get()         ## if the inversion has not been cached, do the calculation
         inver <- solve(matrix, ...)
         x$setinversion(inver)
-        inver
+        inver   ## Return a matrix that is the inverse of 'x'
 }
